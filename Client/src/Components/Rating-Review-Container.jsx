e1f79a72a211e6d74386eaf9b7e536b63a0db07e
Jordan <jordslaptop@Jordans-MacBook-Pro.local>
2022-12-14 09:45:14 -0800

    Created css file for Rating & Review Parent Component

--- Client/src/Components/Rating-Review-Container.jsx
+++ Client/src/Components/Rating-Review-Container.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Ratings from "./Ratings";
 import Reviews from "./Reviews";
+import "./Ratings-Review.css";
+
 
 const Ratings_Review_Container = (props) => {
     return (
