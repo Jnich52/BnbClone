b4fc7d474b5e85f7dbe982cd34de02ded0a40628
ghribik <g.hribik@gmail.com>
2022-12-14 16:44:07 -0800

    finished the shell for the Rerservation and checkout card

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,14 +2,15 @@ import React, {useEffect, useState} from 'react';
 import Ratings_Review_Container from './Components/Rating-Review-Container';
 import Reservation from './Components/Reservation.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Components/Reservation.css'
 
 function App() {
   
 
   return (
     <div className="App">
-    <Ratings_Review_Container />
     <Reservation></Reservation>
+    <Ratings_Review_Container />
     </div>
   )
 }
