0c0515a3e0e1c629eb8f324aa9dff526de4f85db
VitaVelMors <bwhitemedic@icloud.com>
2022-12-19 16:34:53 -0600

    Modal UI fixes

--- Client/src/Components/AboutThisSpace-Modal.jsx
+++ Client/src/Components/AboutThisSpace-Modal.jsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import { Card, Button, Modal, CloseButton } from 'react-bootstrap';
 
 const AboutThisSpaceModal = () => {
-  
+
   const [show, setShow] = useState(false);
   
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+
   return (
     <Card className='property-card'>
+      
       <Card.Body><p>
             Come make long-lasting memories by the lake, take in these amazing sunsets and enjoy 
             lots of fun activities for the whole family - hiking, kayaking, horseback riding, swimming.
@@ -17,26 +19,34 @@ const AboutThisSpaceModal = () => {
           <p>
             Relax in this amazing Tree House right on Sayersbrook lake. You’ll have a private hot tub, 
             fireplace, fully furnished kitchen, beautiful handmade furniture, huge deck, and yes, those 
-            views of the 60-acre lake!!
+            views of the 60-acre lake!!...
           </p></Card.Body>
-      <Button variant='link' className='aircover-btn' onClick={handleShow}>
-        Show more
-      </Button>
-      <Modal show={show} size='xl' onHide={handleClose}>
+
+          <Button  variant='link' className='aircover-btn' onClick={handleShow}>
+            Show more
+            <svg viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false" 
+            style={{height: '12px', width: '12px', display: 'flex', fill: '#222222', marginLeft:'.25rem'}}>
+            <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" 
+            fill-rule="evenodd"></path></svg>
+          </Button>
+
+      <Modal show={show} size='lg' fullscreen='lg-down' style={{color: '#222222', fontSize: '16px !important',}} onHide={handleClose}>
+        
         <Modal.Header className='modal-header' closeButton>
         </Modal.Header>
+        <Modal.Title>About this space</Modal.Title>
         <Modal.Body className='modal-body'>
-          <h2>About this space</h2>
           <p>
             Come make long-lasting memories by the lake, take in these amazing sunsets and enjoy 
             lots of fun activities for the whole family - hiking, kayaking, horseback riding, swimming.
           </p>
+          <br/>
           <p>
             Relax in this amazing Tree House right on Sayersbrook lake. You’ll have a private hot tub, 
             fireplace, fully furnished kitchen, beautiful handmade furniture, huge deck, and yes, those 
             views of the 60-acre lake!!
           </p>
-          
+          <br/>
           <h4>The space</h4>
           <p>
           Dancing under Red Skies is a beautiful fully furnished luxury treehouse elevated 12 feet above 
@@ -49,10 +59,13 @@ const AboutThisSpaceModal = () => {
           Includes a luxury kitchen with a full-size fridge and oven for cooking and preparing a meal. This 
           treehouse offers so many more amenities, not enough space to mention here, we would love it if you 
           could experience this dream; book today!
+          <br/>
           Horseback riding, kayak rental, hiking trails are available.
           </p>
         </Modal.Body>
+      
       </Modal>
+    
     </Card>
   );
 }
--- Client/src/Components/AirCover-Modal.jsx
+++ Client/src/Components/AirCover-Modal.jsx
@@ -21,23 +21,30 @@ const AirCoverModal = () => {
         Learn more
       </Button>
 
-      <Modal id='aircover-modal' size='xl' scrollable centered show={show} onHide={handleClose}>
+      <Modal id='aircover-modal' size='xl' fullscree='lg-down' style={{color: '#717171',}} scrollable centered show={show} onHide={handleClose}>
         <Modal.Header closeButton>
         </Modal.Header>
         <Modal.Body className='modal-body'>
+          <div className='first-para'>
             <img className='modal-img' src="https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg" alt="AirCover"/>
-            <p className='first-paragraph'>
+            <p>
               AirCover is comprehensive protection included for free with every booking.
             </p>
+            </div>
+            <div className='modal-para'>
             <h4>Booking Protection Guarantee</h4>
             <p>
               In the unlikely event a Host needs to cancel your booking within 30 days of check-in, we’ll find you a similar or better home, or we’ll refund you.
             </p>
+            </div>
+            <div className='modal-para'>
             <h4>Check-In Guarantee</h4>
             <p>
               If you can’t check into your home and the Host cannot resolve the issue, 
               we’ll find you a similar or better home for the length of your original stay, or we’ll refund you.
             </p>
+            </div>
+            <div className='modal-para'>
             <h4>Get-What-You-Booked Guarantee</h4>
             <p>
               If at any time during your stay you find your listing isn't as advertised—for example, 
@@ -45,14 +52,18 @@ const AirCoverModal = () => {
               or it has fewer bedrooms than listed—you'll have three days to report it 
               and we’ll find you a similar or better home, or we’ll refund you.
             </p>
+            </div>
+            <div className='modal-para'>
             <h4>24-hour Safety Line</h4>
             <p>
               If you ever feel unsafe, you’ll get priority access to specially-trained safety agents, day or night.
             </p>
-            <p>
+            </div>
+            <p className='last-line'>
               Find complete details on how AirCover protects your booking in our{' '}
             <a target="_blank" href="https://www.airbnb.com/help/article/3218">Help Center</a>
             </p>
+            
         </Modal.Body>
       </Modal>
     </Card>
